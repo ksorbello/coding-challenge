9295fe9bc76257b4256c376f7555cea4d7c74800
Theodore Moreland <theodoremoreland@yahoo.com>
2021-02-25 15:33:32 -0600

    Added python solutions for initial 7 challenges

--- python/rnaTranscription/rna_transcription.py
+++ python/rnaTranscription/rna_transcription.py
@@ -1,2 +1,7 @@
 def to_rna(dna_strand):
-    pass
+    dna_strand = dna_strand.replace("G", "c")
+    dna_strand = dna_strand.replace("C", "g")
+    dna_strand = dna_strand.replace("T", "a")
+    dna_strand = dna_strand.replace("A", "u")
+    dna_strand = dna_strand.upper()
+    return dna_strand

3b91ae46d6a4e1e984bc00ccbcae232220e19290
Theodore Moreland <theodoremoreland@yahoo.com>
2021-02-26 14:34:13 -0600

    Updated README

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -3,28 +3,6 @@
 // convenience to get you started writing code faster.
 //
 
-export const compute = (left, right) => {
-  let hamming = 0;
-  if (!right && left) {
-    throw new Error(errors.rightEmpty);
-  } else if (!left && right) {
-    throw new Error(errors.leftEmpty);
-  } else if (right.length !== left.length) {
-    throw new Error(errors.diflength);
-  } else {
-    left = left.split("");
-    right = right.split("");
-    for (let i = 0; i < left.length; i++) {
-      if (left[i] !== right[i]) {
-        hamming++;
-      }
-    }
-  }
-  return hamming;
-};
-
-const errors = {
-  rightEmpty: "right strand must not be empty",
-  leftEmpty: "left strand must not be empty",
-  diflength: "left and right strands must be of equal length",
+export const compute = () => {
+  throw new Error('Remove this statement and implement this function');
 };
